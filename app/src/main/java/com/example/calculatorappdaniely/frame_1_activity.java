7b9b63428f2039996973609b27af47d9ad5c3bad
yoondaniel1 <yoondaniel2006@gmail.com>
2023-09-08 08:22:29 -0500

    worked on dimensions 9/8/23
    made the buttons and backgrounds fit the dimensions of the pixel 4 (393 x 829)

---
+++ app/src/main/java/com/example/calculatorappdaniely/frame_1_activity.java
@@ -0,0 +1,118 @@
+package com.example.calculatorappdaniely;
+
+/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		frame_1
+	 *	@date 		Thursday 07th of September 2023 04:07:29 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class frame_1_activity extends Activity {
+
+	
+	private View _bg__frame_1;
+	private View rectangle_1;
+	private View rectangle_2;
+	private View circle;
+	private View circle_ek1;
+	private View circle_ek2;
+	private View circle_ek3;
+	private View circle_ek4;
+	private View circle_ek5;
+	private View circle_ek6;
+	private View circle_ek7;
+	private View circle_ek8;
+	private View circle_ek9;
+	private View circle_ek10;
+	private View circle_ek11;
+	private View circle_ek12;
+	private View circle_ek13;
+	private View circle_ek14;
+	private View rectangle_3;
+	private View ellipse_1;
+	private View ellipse_2;
+	private TextView __;
+	private TextView _0;
+	private TextView _1;
+	private TextView _2;
+	private TextView _3;
+	private TextView _6;
+	private TextView _5;
+	private TextView _4;
+	private TextView _7;
+	private TextView _8;
+	private TextView _9;
+	private ImageView shape;
+	private ImageView shape_ek1;
+	private ImageView shape_ek2;
+	private ImageView shape_ek3;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.frame_1);
+
+		
+		_bg__frame_1 = (View) findViewById(R.id._bg__frame_1);
+		rectangle_1 = (View) findViewById(R.id.rectangle_1);
+		rectangle_2 = (View) findViewById(R.id.rectangle_2);
+		circle = (View) findViewById(R.id.circle);
+		circle_ek1 = (View) findViewById(R.id.circle_ek1);
+		circle_ek2 = (View) findViewById(R.id.circle_ek2);
+		circle_ek3 = (View) findViewById(R.id.circle_ek3);
+		circle_ek4 = (View) findViewById(R.id.circle_ek4);
+		circle_ek5 = (View) findViewById(R.id.circle_ek5);
+		circle_ek6 = (View) findViewById(R.id.circle_ek6);
+		circle_ek7 = (View) findViewById(R.id.circle_ek7);
+		circle_ek8 = (View) findViewById(R.id.circle_ek8);
+		circle_ek9 = (View) findViewById(R.id.circle_ek9);
+		circle_ek10 = (View) findViewById(R.id.circle_ek10);
+		circle_ek11 = (View) findViewById(R.id.circle_ek11);
+		circle_ek12 = (View) findViewById(R.id.circle_ek12);
+		circle_ek13 = (View) findViewById(R.id.circle_ek13);
+		circle_ek14 = (View) findViewById(R.id.circle_ek14);
+		rectangle_3 = (View) findViewById(R.id.rectangle_3);
+		ellipse_1 = (View) findViewById(R.id.ellipse_1);
+		ellipse_2 = (View) findViewById(R.id.ellipse_2);
+		__ = (TextView) findViewById(R.id.__);
+		_0 = (TextView) findViewById(R.id._0);
+		_1 = (TextView) findViewById(R.id._1);
+		_2 = (TextView) findViewById(R.id._2);
+		_3 = (TextView) findViewById(R.id._3);
+		_6 = (TextView) findViewById(R.id._6);
+		_5 = (TextView) findViewById(R.id._5);
+		_4 = (TextView) findViewById(R.id._4);
+		_7 = (TextView) findViewById(R.id._7);
+		_8 = (TextView) findViewById(R.id._8);
+		_9 = (TextView) findViewById(R.id._9);
+		shape = (ImageView) findViewById(R.id.shape);
+		shape_ek1 = (ImageView) findViewById(R.id.shape_ek1);
+		shape_ek2 = (ImageView) findViewById(R.id.shape_ek2);
+		shape_ek3 = (ImageView) findViewById(R.id.shape_ek3);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
